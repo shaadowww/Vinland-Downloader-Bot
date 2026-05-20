4d8e3b8c2753d2ca1556448295eb52caaabdd1b0
shaadowww <ppupkin1337@gmail.com>
2026-05-21 01:21:48 +0300

    improved app.py, changed config.py

--- bot/app.py
+++ bot/app.py
@@ -1,61 +1,106 @@
 import logging
 
-from aiogram import Bot, Dispatcher, Router
-from aiogram.types import Message
-from aiogram.filters import Command
-from aiogram.types import FSInputFile
 import asyncio
 
-from dotenv import load_dotenv
-import os
+from typing import Any, Awaitable, Callable, Dict
+
+from aiogram import BaseMiddleware, Bot, Dispatcher, Router
+from aiogram.exceptions import TelegramForbiddenError
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.types import FSInputFile, TelegramObject
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.schemas import UserCreate, UserUpdate
+from database.db_engines import sessionmaker, engine
+from database.core import set_user_active_status, upsert_user
+
+from config import settings
 
 from bot.backend import FakeDownloader
-
-load_dotenv()
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN is missing")
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=settings.BOT_TOKEN)
 dp = Dispatcher()
 router = Router()
 downloader = FakeDownloader(workers=3, queue_size=10)
 
+class DbSessionMiddleware(BaseMiddleware):
+    def __init__(self, session_pool: Any):
+        super().__init__()
+        self.session_pool = session_pool
+
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any]
+    ) -> Any:
+        
+        async with self.session_pool() as session:
+            data["session"] = session 
+            return await handler(event, data)
+
+
+# Регистрируем мидлварь для всех текстовых сообщений роутера
+router.message.middleware(DbSessionMiddleware(sessionmaker))
+
 
 @router.message(Command('start'))
-async def send_welcome(message: Message):
+async def send_welcome(message: Message, session: AsyncSession):
     """
-    Hello message on `/start` or `/help` command
+    Welcome message on `/start`
     """
+    user = UserCreate(
+        telegram_id=message.from_user.id,
+        username=message.from_user.username
+    )
+    res = await upsert_user(session, user)
+    logging.info(f"Upsert result: {res}")
+
     await message.answer("Hi!\nI'm vinland downloader bot.")
 
+@router.message(Command('help'))
+async def send_help(msg: Message):
+    pass
+
+@router.message(Command('settings'))
+async def settings_setup(msg: Message, session: AsyncSession):
+    pass
+
 @router.message(Command('run'))
-async def bot_get_results(message: Message):
+async def bot_get_results(message: Message, session: AsyncSession):
     """
     Get results per user id
     """
+    try:
+        userid = message.from_user.id
+        await message.answer(f"Starting downloads...")
+        
+        results = await downloader.get_result(userid)
 
-    await message.answer(f"Starting downloads...")
-    
-    results = await downloader.get_result(message.from_user.id)
+        await message.answer(f"Results: {results}")
+        video = FSInputFile(results[0])
 
-    await message.answer(f"Results: {results}")
-    video = FSInputFile(results[0])
-
-    await message.answer_video(video)
+        await message.answer_video(video)
+    except TelegramForbiddenError:
+        await set_user_active_status(session, userid, False)
 
 
 @router.message()
-async def bot_add_task(message: Message):
-    await downloader.add_url(
-        message.text,
-        message.from_user.id
-    )
-    await message.answer(f"URL added!")
+async def bot_add_task(message: Message, session: AsyncSession):
+    try:
+        await downloader.add_url(
+            message.text,
+            message.from_user.id
+        )
+        await message.answer(f"URL added!")
+    except TelegramForbiddenError:
+        await set_user_active_status(session, message.from_user.id, False)
 
 
 dp.include_router(router)
--- config.py
+++ config.py
@@ -21,6 +21,6 @@ class Settings(BaseSettings):
         # postgresql+asyncpg://USER:PASSWORD@HOST:PORT/DATABASE
         return f"postgresql+asyncpg://{self.DB_USERNAME}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
     
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 settings = Settings()
