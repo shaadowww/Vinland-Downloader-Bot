f07e202cf55524830cb588b88ec22cba70a1d18c
shaadowww <ppupkin1337@gmail.com>
2026-05-23 15:33:27 +0300

    changed bot/

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,12 +1,13 @@
 from dotenv import load_dotenv
 import os
+import logging
 
 from typing import Any, Awaitable, Callable, Dict
 
 from aiogram import Router , F, BaseMiddleware
 from aiogram.types import Message , CallbackQuery
 from aiogram.filters import Command
-import keyboards as kb
+import bot.keyboards as kb
 
 from aiogram.exceptions import TelegramForbiddenError
 from aiogram.types import CallbackQuery, Message
@@ -20,11 +21,18 @@ from database.schemas import UserCreate, UserUpdate
 from database.db_engines import sessionmaker
 from database.core import set_user_active_status, update_user, upsert_user
 
-from backend import FakeDownloader
+from bot.backend import FakeDownloader
 
 valid_url_regex = os.getenv('VALID_URL_REGEX')
 invalid_url_regex = os.getenv('INVALID_URl_REGEX')
 router = Router()
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s [%(asctime)s] - %(message)s",
+    datefmt="%H:%M:%S"
+)
 
 class DbSessionMiddleware(BaseMiddleware):
     def __init__(self, session_pool: Any):
@@ -43,21 +51,21 @@ class DbSessionMiddleware(BaseMiddleware):
             return await handler(event, data)
 
 # Registrate middleware for all text messages
-router.message.middleware(DbSessionMiddleware(sessionmaker))
-router.callback_query.middleware(DbSessionMiddleware(sessionmaker))
+# router.message.middleware(DbSessionMiddleware(sessionmaker))
+# router.callback_query.middleware(DbSessionMiddleware(sessionmaker))
     
 
 @router.message(Command('start'))
-async def send_welcome(message: Message, session: AsyncSession):
+async def send_welcome(message: Message):
     """
     Welcome message on `/start`
     """
-    user = UserCreate(
-        telegram_id=message.from_user.id,
-        username=message.from_user.username
-    )
-    res = await upsert_user(session, user)
-    logging.info(f"Upsert result: {res}")
+    # user = UserCreate(
+    #     telegram_id=message.from_user.id,
+    #     username=message.from_user.username
+    # )
+    # res = await upsert_user(session, user)
+    # logging.info(f"Upsert result: {res}")
 
     await message.answer(
         text="<b>Hi!\nI'm vinland downloader bot.n\n🏞️ I'll help you to download video/music from Youtube/Soundcloud</b>",
@@ -85,55 +93,55 @@ async def send_help(msg: Message):
     await msg.answer(help_text, parse_mode="HTML")
 
 
-@router.message(Command('settings'))
-async def show_settings(msg: Message):
-    '''
-    Displays configuration menu `/settings`
-    '''
+# @router.message(Command('settings'))
+# async def show_settings(msg: Message):
+#     '''
+#     Displays configuration menu `/settings`
+#     '''
 
-    builder = InlineKeyboardBuilder()
+#     builder = InlineKeyboardBuilder()
 
-    builder.button(text="360p", callback_data=kb.QualityCallback(quality="360p"))
-    builder.button(text="480p", callback_data=kb.QualityCallback(quality="480p"))
-    builder.button(text="720p", callback_data=kb.QualityCallback(quality="720p"))
-    builder.button(text="1080p", callback_data=kb.QualityCallback(quality="1080p"))
-    builder.button(text="Always Ask", callback_data=kb.QualityCallback(quality="ask"))
+#     builder.button(text="360p", callback_data=kb.QualityCallback(quality="360p"))
+#     builder.button(text="480p", callback_data=kb.QualityCallback(quality="480p"))
+#     builder.button(text="720p", callback_data=kb.QualityCallback(quality="720p"))
+#     builder.button(text="1080p", callback_data=kb.QualityCallback(quality="1080p"))
+#     builder.button(text="Always Ask", callback_data=kb.QualityCallback(quality="ask"))
 
-    builder.adjust(4, 1)
+#     builder.adjust(4, 1)
 
-    await msg.answer(
-        text="<b>⚙️ Settings</b>\n\nSelect your default video download quality:",
-        reply_markup=builder.as_markup(),
-        parse_mode="HTML"
-    )
+#     await msg.answer(
+#         text="<b>⚙️ Settings</b>\n\nSelect your default video download quality:",
+#         reply_markup=builder.as_markup(),
+#         parse_mode="HTML"
+#     )
 
 
-@router.callback_query(kb.QualityCallback.filter())
-async def quality_selection_callback(
-    callback: CallbackQuery,
-    callback_data: kb.QualityCallback,
-    session: AsyncSession
-):
-    '''
-    Processing quality callback selection
-    '''
+# @router.callback_query(kb.QualityCallback.filter())
+# async def quality_selection_callback(
+#     callback: CallbackQuery,
+#     callback_data: kb.QualityCallback,
+#     session: AsyncSession
+# ):
+#     '''
+#     Processing quality callback selection
+#     '''
 
-    telegram_id = callback.from_user.id
-    selected_quality = UserUpdate(
-        download_quality=callback_data.quality
-    )
+#     telegram_id = callback.from_user.id
+#     selected_quality = UserUpdate(
+#         download_quality=callback_data.quality
+#     )
 
-    await update_user(session, telegram_id, selected_quality)
+#     await update_user(session, telegram_id, selected_quality)
 
-    await callback.answer(
-        text=f"Quality set to {callback_data.quality}"
-    )
+#     await callback.answer(
+#         text=f"Quality set to {callback_data.quality}"
+#     )
 
-    readable_quality = "Always ask " if callback_data.quality == "ask" else callback_data.quality
-    await callback.message.edit_text(
-        text=f"<b> ✓ Settings Saved</b>\n\nDefault download quality updated to: <b>{readable_quality}</b>",
-        parse_mode="HTML"
-    )
+#     readable_quality = "Always ask" if callback_data.quality == "ask" else callback_data.quality
+#     await callback.message.edit_text(
+#         text=f"<b> ✓ Settings Saved</b>\n\nDefault download quality updated to: <b>{readable_quality}</b>",
+#         parse_mode="HTML"
+#     )
 
 
 @router.message(F.data == "Run")
@@ -141,30 +149,30 @@ async def bot_get_results(message: Message, downloader: FakeDownloader):
     """
     Get results per user id
     """
-    try:
-        userid = message.from_user.id
-        await message.answer(f"Starting downloads...")
-        
-        results = await downloader.get_result(userid)
+    # try:
+    userid = message.from_user.id
+    await message.answer(f"Starting downloads...")
+    
+    results = await downloader.get_result(userid)
 
-        await message.answer(f"Results: {results}")
-        video = FSInputFile(results[0])
+    await message.answer(f"Results: {results}")
+    video = FSInputFile(results[0])
 
-        await message.answer_video(video)
-    except TelegramForbiddenError:
-        await set_user_active_status(session, userid, False)
+    await message.answer_video(video)
+    # except TelegramForbiddenError:
+    #     await set_user_active_status(session, userid, False)
 
 
 @router.message()
 async def bot_add_task(message: Message, downloader: FakeDownloader):
-    try:
-        await downloader.add_url(
-            message.text,
-            message.from_user.id
-        )
-        await message.answer(f"URL added!")
-    except TelegramForbiddenError:
-        await set_user_active_status(session, message.from_user.id, False)
+    # try:
+    await downloader.add_url(
+        message.text,
+        message.from_user.id
+    )
+    await message.answer(f"URL added!")
+    # except TelegramForbiddenError:
+    #     await set_user_active_status(session, message.from_user.id, False)
 
 
 @router.message(F.text.regexp(invalid_url_regex)) 
